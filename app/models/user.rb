c55b2eafd744ac7fb747aecea8ebd814f23a7ae0
hanamoto <ruki_107414@mineo.jp>
2019-06-23 10:35:00 +0000

    second

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,11 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   
-  VAILD_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,32}+\z/
-  validates :password, presence: true, confirmation: true, format: { with: VAILD_PASSWORD_REGEX }
+  VAILD_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]+\z/
+  validates :password, presence: true, format: { with: VAILD_PASSWORD_REGEX }
+  validates :password, length: {in: 8..32}
   
-    validates :password_confirmation, presence: true
+  validates :password_confirmation, presence: true
   
   has_secure_password
   
